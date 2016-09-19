6f19abcfa4fa06ffb7df977f3d2c639c82346ea8
Basani Rakesh <basanirakesh@gmail.com>
2016-09-19 22:24:25 +0530

    changing customerService to 1

--- angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/customer/CustomerController.java
+++ angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/customer/CustomerController.java
@@ -13,23 +13,23 @@ import java.util.List;
 public class CustomerController {
 
 	@Autowired
-	private CustomerService customerService;
+	private CustomerService customerService1;
 	
 	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/customers/retrieve", method = RequestMethod.GET, produces = "application/json")
 	public List<Customer> getCustomers() {
-		return customerService.getCustomers();
+		return customerService1.getCustomers();
 	}
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
     public boolean deleteCustomer(@PathVariable Long id) {
-        return customerService.deleteCustomer(id);
+        return customerService1.deleteCustomer(id);
     }
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/save", method = RequestMethod.POST, produces = "application/json")
     public boolean saveCustomer(@RequestBody Customer customer) {
-        return customerService.saveCustomer(customer);
+        return customerService1.saveCustomer(customer);
     }
 }
