cbb37e8eeab380db53a276004317d0ee9c23528a
Basani Rakesh <rakesh.basani@adp.com>
2016-09-27 14:46:26 +0530

    changing logger 37 and adding comment

--- angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/authentication/AuthenticationController.java
+++ angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/authentication/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/auth")
 public class AuthenticationController {
 
- private static Logger log35 = LoggerFactory.getLogger(AuthenticationController.class);
+ private static Logger log37 = LoggerFactory.getLogger(AuthenticationController.class);
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/login", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -21,8 +21,8 @@ public class AuthenticationController {
 
     @RequestMapping(value = "/logout/validate", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
     public void logout(@RequestParam(value = "status") String status) {
-        if(log35.isDebugEnabled()) {
-            log35.debug("Logout status " + status);
+        if(log37.isDebugEnabled()) {
+            log37.debug("Logout status " + status);
         }
     }
 }
--- angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/user/UserController.java
+++ angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+	//Comment
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/retrieve", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
     public UserDetail retrieveUser(@AuthenticationPrincipal SpringUserDetail user) {
