c86d10dfcceb11ac15a236d82a64e3a12026e46d
Basani Rakesh <rakesh.basani@adp.com>
2016-09-27 13:55:55 +0530

    changing logger to log31

--- angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/authentication/AuthenticationController.java
+++ angular_bootstrap_spring/api/src/main/java/au/com/example/api/controller/authentication/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/auth")
 public class AuthenticationController {
 
- private static Logger log30 = LoggerFactory.getLogger(AuthenticationController.class);
+ private static Logger log31 = LoggerFactory.getLogger(AuthenticationController.class);
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/login", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -21,8 +21,8 @@ public class AuthenticationController {
 
     @RequestMapping(value = "/logout/validate", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
     public void logout(@RequestParam(value = "status") String status) {
-        if(log30.isDebugEnabled()) {
-            log30.debug("Logout status " + status);
+        if(log31.isDebugEnabled()) {
+            log31.debug("Logout status " + status);
         }
     }
 }
